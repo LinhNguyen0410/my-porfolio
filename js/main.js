5d9e915e25f04b9490b2070d79749d2c93c68f53
Linh Nguyen <linhgbale0410@gmail.com>
2022-01-09 22:35:45 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-// const $ = document.querySelector.bind(document);
-// const $$ = document.querySelectorAll.bind(document);
-// // ----------------------------
-// const header = $(".header");
+const $$ = document.querySelector.bind(document);
+const $$$ = document.querySelectorAll.bind(document);
+// ----------------------------
+const header = $$(".header");
 
-// function handleChangeHeaderWhenScrolled() {
-//   if (!header) return;
-//   window.onscroll = function () {
-//     const positonHeader = window.pageYOffset;
-//     if (positonHeader !== 0) {
-//       header.style.top = 0;
-//       header.style.backgroundColor = "#fff";
-//       header.style.transition = "all .2s linear";
-//       header.style.boxShadow = "0  1px 2px #ccc";
-//     } else {
-//       header.style.top = "25px";
-//       header.style.backgroundColor = "transparent";
-//       header.style.transition = "all .2s linear";
-//       header.style.boxShadow = "none";
-//     }
-//   };
-// }
-// (() => {
-//   handleChangeHeaderWhenScrolled();
-// })();
+function handleChangeHeaderWhenScrolled() {
+  if (!header) return;
+  window.onscroll = function () {
+    const positonHeader = window.pageYOffset;
+    if (positonHeader !== 0) {
+      header.style.top = 0;
+      header.style.backgroundColor = "#fff";
+      header.style.transition = "all .2s linear";
+      header.style.boxShadow = "0  1px 2px #ccc";
+    } else {
+      header.style.top = "25px";
+      header.style.backgroundColor = "transparent";
+      header.style.transition = "all .2s linear";
+      header.style.boxShadow = "none";
+    }
+  };
+}
+
+(() => {
+  handleChangeHeaderWhenScrolled();
+})();
+
 $(document).ready(function () {
   $(".activity-slider").slick({
     slidesToShow: 2,
